25c049d2079732f3534b59233f13e1ed992671db
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-19 11:11:48 -0300

    feat: trabalhando com single thread

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
+import imprimeCotacao from "./imprimeCotacao.js";
+
 const graficoDolar = document.getElementById('graficoDolar');
 
 const graficoParaDolar = new Chart(graficoDolar, {
-        type: 'line',
-        data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    type: 'line',
+    data: {
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Dólar',
+            data: [],
             borderWidth: 1
         }]
     },
@@ -17,14 +19,24 @@ setInterval(() => conectaAPI(), 5000);
 async function conectaAPI() {
     const conecta = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL');
     const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
+    let tempo = geraHorario();
+    let valor = conectaTraduzido.USDBRL.ask;
+    adicionarDados(graficoParaDolar, tempo, valor);
+    imprimeCotacao('dolar', valor);
 }
 
-function gerarHorario() {
+function geraHorario() {
     let data = new Date();
     let horario = data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds();
     console.log(horario);
-    console.log(data);
+    return horario;
 }
 
-gerarHorario();
+function adicionarDados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda);
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados);
+    });
+
+    grafico.update();
+}
